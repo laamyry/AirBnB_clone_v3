7f593d99cfccde22320432f4056c67e68a02c312
laamyry <slamiri10@gmail.com>
2023-10-29 00:59:28 +0100

    AirBnB_clone_v3

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -74,3 +74,22 @@ class DBStorage:
     def close(self):
         """call remove() method on the private session attribute"""
         self.__session.remove()
+
+    def get(self, cls, id):
+        """Returns the object based on the class and
+        its ID, or None if not found"""
+        objets = self.all(cls)
+        for objet in objets:
+            if objet.id == id:
+                return objet
+        return None
+
+    def count(self, cls=None):
+        """Returns the number of objects in storage matching the given class.
+        If no class is passed, returns the count of all objects in storage."""
+        if cls is None:
+            return len(self.all())
+        else:
+            return len(self.all(cls))
+
+    
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -68,3 +68,20 @@ class FileStorage:
     def close(self):
         """call reload() method for deserializing the JSON file to objects"""
         self.reload()
+
+    def get(self, cls, id):
+        """Returns the object based on the class and
+        its ID, or None if not found"""
+        objets = self.all(cls)
+        for objet in objets:
+            if objet.id == id:
+                return objet
+        return None
+
+    def count(self, cls=None):
+        """Returns the number of objects in storage matching the given class.
+        If no class is passed, returns the count of all objects in storage."""
+        if cls is None:
+            return len(self.all())
+        else:
+            return len(self.all(cls))
