5cf6136f283fb62d39875e94d0de51e7f6811ab2
laamyry <slamiri10@gmail.com>
2023-10-30 00:24:08 +0100

    AirBnB_clone_v3

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,3 +1,3 @@
-from flask import Blueprint as print
-app_views = print('app_views', __name__, url_prefix='/api/v1')
+from flask import Blueprint as bprint
+app_views = bprint('app_views', __name__, url_prefix='/api/v1')
 from api.v1.views.index import *
