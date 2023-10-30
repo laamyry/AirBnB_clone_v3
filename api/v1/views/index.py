a5811ae32795070670a6cd8bb7508fa65e0daf5b
laamyry <slamiri10@gmail.com>
2023-10-30 03:18:14 +0100

    laamyry py

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,9 +2,27 @@
 '''Status of your API'''
 from api.v1.views import app_views as views
 from flask import jsonify as json
+from models import storage
 
 
 @views.route('/status', methods=['GET'], strict_slashes=False)
-def status():
+def getstatus():
     """status"""
     return json({"status": "OK"})
+
+
+def getstats():
+    """stats"""
+    amen = storage.count("Amenity")
+    citi = storage.count("City")
+    plac = storage.count("Place")
+    revi = storage.count("Review")
+    stat = storage.count("State")
+    user = storage.count("User")
+
+    stas = {
+        "amenities": amen, "cities": citi,
+        "places": plac, "reviews": revi,
+        "states": stat, "users": user}
+
+    return json(stas)
