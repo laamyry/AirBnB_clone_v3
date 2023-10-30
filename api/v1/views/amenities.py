bbd344beeb4ae56e4acdf0250d294a3ce25a2770
laamyry <slamiri10@gmail.com>
2023-10-30 23:36:45 +0100

    laamyry py

--- api/v1/views/amenities.py
+++ api/v1/views/amenities.py
@@ -9,8 +9,10 @@ from models.amenity import Amenity
 @views.route('/amenities', methods=['GET'], strict_slashes=False)
 def getAmenities():
     """Retrieves list all Amenity"""
-    amenities = [amenity.to_dict() for amenity in storage.all(Amenity).values()]
+    amenities = [amenity.to_dict()
+                 for amenity in storage.all(Amenity).values()]
     return json(amenities)
+
 
 @views.route('/amenities/<amenity_id>', methods=['GET'], strict_slashes=False)
 def getAamenity(amenity_id):
@@ -19,6 +21,7 @@ def getAamenity(amenity_id):
     if amenity is None:
         abort(404)
     return json(amenity.to_dict())
+
 
 @views.route('/amenities/<amenity_id>', methods=['DELETE'], strict_slashes=False)
 def delete_amenity(amenity_id):
@@ -29,6 +32,7 @@ def delete_amenity(amenity_id):
     storage.delete(amenity)
     storage.save()
     return json({})
+
 
 @views.route('/amenities', methods=['POST'], strict_slashes=False)
 def create_amenity():
@@ -41,6 +45,7 @@ def create_amenity():
     new_amenity = Amenity(**data)
     new_amenity.save()
     return json(new_amenity.to_dict()), 201
+
 
 @views.route('/amenities/<amenity_id>', methods=['PUT'], strict_slashes=False)
 def update_amenity(amenity_id):
