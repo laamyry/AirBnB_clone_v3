4c89fc97db63ae6cd9abc68838a5b32f84e1d098
laamyry <slamiri10@gmail.com>
2023-10-30 19:33:34 +0100

    laamyry py

--- api/v1/app.py
+++ api/v1/app.py
@@ -14,6 +14,10 @@ def close_storage(exception):
     """close storage"""
     storage.close()
 
+@app.errorhandler(404)
+def not_found(error):
+    """error"""
+    return {"error": "Not found"}, 404
 
 if __name__ == "__main__":
     host = getenv('HBNB_API_HOST', "0.0.0.0")
