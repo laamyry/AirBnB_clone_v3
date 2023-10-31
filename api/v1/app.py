09bb723d3912010e1357c175dea2bea09c6dc6ee
saad ouchama <saadochama@gmail.com>
2023-10-30 17:38:05 -0700

    add task 12.HTTP access control (CORS)

--- api/v1/app.py
+++ api/v1/app.py
@@ -4,16 +4,21 @@ from flask import Flask
 from models import storage
 from api.v1.views import app_views as views
 from os import getenv
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 app.register_blueprint(views)
-
 
 @app.teardown_appcontext
 def close_storage(exception):
     """close storage"""
     storage.close()
 
+@app.errorhandler(400)
+def page_not_found(e):
+    message = e.description
+    return message, 400
 
 @app.errorhandler(404)
 def not_found(error):
